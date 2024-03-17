db9c66165f205fd21c65acbd3607d9ca1c447214
Christiane <christiane@Mac-mini-de-WESLEI.local>
2024-03-16 22:19:11 -0300

    deleta classes

--- src/main/java/br/com/rm97465/medbuscaapi/repository/EmpRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.rm97465.medbuscaapi.repository;
-
-import br.com.rm97465.medbuscaapi.model.Emp;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpRepository extends JpaRepository<Emp, Integer> {
-}
