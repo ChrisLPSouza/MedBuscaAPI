b3305ff1e97c9df04fe4df4dae236f90d6f511f4
Ana Sá <anasadevweb@gmail.com>
2024-06-12 22:51:35 +0200

    Supressão classe FormBusca

--- src/main/java/br/com/rm97465/medbuscaapi/controller/HomeController.java
+++ src/main/java/br/com/rm97465/medbuscaapi/controller/HomeController.java
@@ -1,14 +1,11 @@
 package br.com.rm97465.medbuscaapi.controller;
 
-import br.com.rm97465.medbuscaapi.model.Farmacia;
-import br.com.rm97465.medbuscaapi.model.FormBusca;
 import br.com.rm97465.medbuscaapi.model.Receita;
 import br.com.rm97465.medbuscaapi.webservice.services.FarmaciaService;
 import br.com.rm97465.medbuscaapi.webservice.services.ReceitaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
